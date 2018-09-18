99fda63fee1dd45de6cd5a05544e9d89e4ea6d64
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-18 15:46:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.
-  else
-    ""
-  end
+  # elsif phrase == phrase.
+  # else
+  #   ""
+  # end
 end
