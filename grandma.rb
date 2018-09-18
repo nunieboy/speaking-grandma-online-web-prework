37a47e3677faa7a4ad1e5230dad361944d3436bb
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-18 15:50:25 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
-    "NO, NOT SINCE 1938!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
